e59de41f2bbd0d6bdaa1dc1fe8d4f0b19b65cda8
MrGrey178 <ss4458ss51@gmail.com>
2021-10-24 21:35:54 +0400

    added routes, password hashing and more

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const connectMongo = require('./config/db');
 const dotenv = require('dotenv');
 const methodOverride = require('method-override');
 const exphbs = require('express-handlebars');
@@ -20,8 +19,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // handlebars
-app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    defaultLayout: 'main'
+}));
 app.set('view engine', '.hbs');
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/', require(path.join(__dirname, 'routes', 'index')));
 
 const PORT = process.env.PORT || 5000;
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,78 @@
 const router = require('express').Router();
+const User = require('../schemas/User');
+const bcrypt = require('bcryptjs');
 
+// test page
+// GET request
+router.get('/', async (req, res) => {
+    res.render('test');
+});
 
-// login/register page
+// login page 
+// GET Request
 router.get('/login', async (req, res) => {
-    res.render('login');
-})
+    res.render('login', {
+        layout: 'login'
+    });
+});
+
+// login page 
+// POST Request
+router.post('/login', async (req, res) => {
+    if (req.body) {
+        try {
+            const currentUser = await User.findOne({'name': req.body.login});
+            if (currentUser) {
+                bcrypt.compare(req.body.password, currentUser.password, (err, response) => {
+                    if (response) {
+                        res.redirect('/');
+                    }
+                });
+            } else {
+                res.redirect('login');
+            }
+        } catch (err) {
+            
+        }
+    } else {
+        res.render('error/500');
+    }
+});
+
+// register page
+// GET Request
+router.get('/signup', async (req, res) => {
+    res.render('signup', {
+        layout: 'login'
+    })
+});
+
+// register page
+// POST Request
+router.post('/verifyIdentity', async (req, res) => {
+    try {
+        if (req.body) {
+            bcrypt.genSalt(10, async (err, salt) => {
+                bcrypt.hash(req.body.password, salt, async (err, hash) => {
+                    if (hash) {
+                        await User.create({
+                            name: req.body.name,
+                            email: req.body.email,
+                            password: hash,
+                        });
+                        res.redirect('/forum');
+                    } else {
+                        console.error(err);
+                    }
+                });
+            });
+        } else {
+            
+        }
+    } catch (err) {
+        res.render('error/500');
+        console.error(err);
+    }
+});
+
+module.exports = router;
